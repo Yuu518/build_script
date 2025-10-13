a39510ab664e2e2d4e890306e3a4dfa675b31afa
Yuu518 <50163161+Yuu518@users.noreply.github.com>
2025-10-13 14:12:26 +0900

    Custom

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-# 自定义脚本
+# add nikki
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/new/nikki
+
+# add ddns-go
+git clone https://$github/sirpdboy/luci-app-ddns-go package/new/ddns-go
